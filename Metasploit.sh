727d6ab839832c048c7b5194510cb93b3eda2c1e
2023robo <131851257+2023robo@users.noreply.github.com>
2023-04-26 13:07:01 +0530

    Create Metasploit.sh
    This tool run with termux.

---
+++ Metasploit.sh
@@ -0,0 +1,13 @@
+MetaSploit  -: Installation Commands :-
+
+--------------------------------------------------------------------
+
+pkg update && pkg upgrade 
+
+pkg install git curl wget nmap -y
+
+curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+chmod 777 metasploit.sh
+
+./metasploit.sh
